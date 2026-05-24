667d9e77a4ba3494aca1ff2ac53cd7cc4b767dfa
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2026-05-24 03:27:59 -0300

    Update Info.php

--- app/code/community/UltraDev/MercadoPago/Block/Info.php
+++ app/code/community/UltraDev/MercadoPago/Block/Info.php
@@ -1,33 +1,9 @@
 <?php
 class UltraDev_MercadoPago_Block_Info extends Mage_Payment_Block_Info
 {
-    protected function _prepareSpecificInformation($transport = null)
+    protected function _construct()
     {
-        $transport = parent::_prepareSpecificInformation($transport);
-        $info      = $this->getInfo();
-        $data      = array();
-
-        $fields = array(
-            'mp_submethod'               => 'Sub-método',
-            'payment_id_detail'          => 'ID Pagamento MP',
-            'status'                     => 'Status MP',
-            'status_detail'              => 'Detalhe MP',
-            'payment_method_id'          => 'Método',
-            'installments'               => 'Parcelas',
-            'trunc_card'                 => 'Cartão',
-            'cardholderName'             => 'Titular',
-            'payer_identification_type'  => 'Tipo Documento',
-            'payer_identification_number'=> 'Documento',
-            'boleto_url'                 => 'URL do Boleto',
-        );
-
-        foreach ($fields as $key => $label) {
-            $val = $info->getAdditionalInformation($key);
-            if (!empty($val)) {
-                $data[$label] = $val;
-            }
-        }
-
-        return $transport->setData(array_merge($data, $transport->getData()));
+        parent::_construct();
+        $this->setTemplate('ultradev/mercadopago/info.phtml');
     }
 }
