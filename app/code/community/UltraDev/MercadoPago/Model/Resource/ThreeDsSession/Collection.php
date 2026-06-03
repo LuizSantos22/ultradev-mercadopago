2442bcb020481c30a4fd64ae6ca82493fd6d4d64
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2026-06-03 00:02:51 -0300

    Delete app/code/community/UltraDev/MercadoPago/Model directory

--- app/code/community/UltraDev/MercadoPago/Model/Resource/ThreeDsSession/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-class UltraDev_MercadoPago_Model_Resource_ThreeDsSession_Collection
-    extends Mage_Core_Model_Resource_Db_Collection_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init('ultradev_mercadopago/threeds_session');
-    }
-}
